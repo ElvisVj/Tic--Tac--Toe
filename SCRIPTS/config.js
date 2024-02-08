5b754106ef55460da57d55cfa49516037512fc92
Elvis Vjuns <evjuns@gmail.com>
2024-02-08 21:12:48 +0200

    Validating user input

--- SCRIPTS/config.js
+++ SCRIPTS/config.js
@@ -3,6 +3,19 @@ function openPlayerConfig() {
   backDropElement.style.display = "block";
 }
 function closePlayerConfig() {
-    playerConfigOverlayElement.style.display = "none";
+  playerConfigOverlayElement.style.display = "none";
     backDropElement.style.display = "none";
+    formElement.firstElementChild.classList.remove("error");
+    errorsOutputElement.textContent = "";
+}
+function savePlayerConfig(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const enteredPlayername = formData.get("playername").trim();
+    if (!enteredPlayername) {
+        event.target.firstElementChild.classList.add("error")
+        errorsOutputElement.textContent = "Please enter a valid name!"
+        return;
+    }
+
 }
