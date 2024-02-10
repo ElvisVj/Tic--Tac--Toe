d9637890a5500b45db0f6e1b2b7ce1731b582d4f
Elvis Vjuns <evjuns@gmail.com>
2024-02-10 15:46:45 +0200

    Checking for winner or DRAW

--- SCRIPTS/config.js
+++ SCRIPTS/config.js
@@ -5,21 +5,23 @@ function openPlayerConfig(event) {
 }
 function closePlayerConfig() {
   playerConfigOverlayElement.style.display = "none";
-    backDropElement.style.display = "none";
-    formElement.firstElementChild.classList.remove("error");
+  backDropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
   errorsOutputElement.textContent = "";
   formElement.firstElementChild.lastElementChild.value = "";
 }
 function savePlayerConfig(event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const enteredPlayername = formData.get("playername").trim();
-    if (!enteredPlayername) {
-        event.target.firstElementChild.classList.add("error")
-        errorsOutputElement.textContent = "Please enter a valid name!"
-        return;
-    }
-  const updatedPlayerDataElement = document.getElementById("player-" + editedPlayer + "-data");
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayername = formData.get("playername").trim();
+  if (!enteredPlayername) {
+    event.target.firstElementChild.classList.add("error");
+    errorsOutputElement.textContent = "Please enter a valid name!";
+    return;
+  }
+  const updatedPlayerDataElement = document.getElementById(
+    "player-" + editedPlayer + "-data"
+  );
   updatedPlayerDataElement.children[1].textContent = enteredPlayername;
 
   players[editedPlayer - 1].name = enteredPlayername;
